ba4b0e02173be63a20f5d29b7cd8c97552ec676d
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-08 11:09:34 +0800

    轉js

--- c01/js/add-content.js
+++ c01/js/add-content.js
@@ -1,2 +1,4 @@
 var my_name = "Jonathan"; /*宣告變數my_name*/
 console.log(my_name);
+var first_name = "Lin";
+console.log(first_name);
--- c01/ts/add-content.ts
+++ c01/ts/add-content.ts
@@ -1,3 +1,5 @@
 let my_name ="Jonathan";  /*宣告變數my_name*/ 
 console.log(my_name);
+let first_name="Lin";
+console.log(first_name);
 
