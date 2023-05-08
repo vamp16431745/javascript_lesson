12c7038c7862cb217ac76c81aad1bbf9724a2d08
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-08 14:18:48 +0800

    建立字串變數 + 指定變數

---
+++ c02/js/string-variable.js
@@ -0,0 +1,9 @@
+"use strict";
+let user_name;
+let message;
+user_name = "林建陞";
+message = "我有點想睡覺";
+let name_span = document.getElementById('name');
+name_span.innerText = user_name;
+let note_div = document.getElementById('note');
+note_div.innerText = message;
