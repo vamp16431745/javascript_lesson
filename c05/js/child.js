92f1e0406eb2af157c8bbf248d8e41e9fd3b261a
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-09 16:28:53 +0800

    使用ul getElementByTagName 更改第1跟第4個classname

---
+++ c05/js/child.js
@@ -0,0 +1,6 @@
+let ul_element=document.getElementsByTagName('ul')[0];
+let first_child=ul_element.firstElementChild;
+let last_child=ul_element.lastElementChild;
+
+first_child.className='complete';
+last_child.className='cool';
