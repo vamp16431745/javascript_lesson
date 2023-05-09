cf6c7a00108d9a1fe2aaa987ff6de315faa1eb25
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-09 11:51:28 +0800

    加入註解

--- c03/js/basic-function.js
+++ c03/js/basic-function.js
@@ -1,8 +1,8 @@
-function updateMessage(message)
+function updateMessage(message1)   //建立一個方法使id為'message'的內容更替為參數
 {
     let message_element=document.getElementById('message');
-    message_element.innerText=message;
+    message_element.innerText=message1;  
 }
 
-let msg="加入會員可以得到10%off的餐券";
+let msg="加入會員可以得到10%off的餐券";  //更改的內容
 updateMessage(msg);
