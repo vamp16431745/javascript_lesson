64566c0e9b9757690159cc6a37605a99dabc96db
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-09 11:19:03 +0800

    modify

--- c02/js/example.js
+++ c02/js/example.js
@@ -1,3 +1,7 @@
-let greeting="Howdy Alopopo";
+let greeting="早安你好";
+let my_name ="A488484844";
+let message=',請確認您的訂單';
+let welcome=greeting + my_name +message;
 let greetingElement= document.getElementById('greeting');
 greetingElement.innerText=greeting;
+greetingElement.innerText=welcome;
