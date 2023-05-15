65d8f613d34fa414014471e5d61936a8c06759d2
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-15 14:53:47 +0800

    456

--- c06/js/event-listener-with-parameters.js
+++ c06/js/event-listener-with-parameters.js
@@ -5,7 +5,6 @@
 
 document.getElementById('username').addEventListener('blur', function (event) {
     let feedbackElement = document.getElementById('feedback')
-
     let username_string = event.target.value
     if (username_string.length < 5) {
         feedbackElement.textContent = "使用者的名稱必需大於5個字元"
