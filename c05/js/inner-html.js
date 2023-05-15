c581c5729117ad75d7a812298dbd2fecb01d44cd
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-15 10:45:27 +0800

    modify

---
+++ c05/js/inner-html.js
@@ -0,0 +1,4 @@
+let oneElement = document.getElementById('one')
+let innerHTML_string =oneElement.innerHTML
+oneElement.innerHTML = 
+'<a href=\"http://www.google.com\">' + innerHtml_string + '</a>'
