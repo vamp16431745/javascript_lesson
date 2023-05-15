3c88ab95e5a47cfb62db10383c50709c2c1086a3
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-15 15:45:54 +0800

    點擊後移除<li>

---
+++ c06/js/event-delegation.js
@@ -0,0 +1,13 @@
+//點擊後移除<li>
+
+document.getElementById('shoppingList')
+    .addEventListener
+    ('click',
+        function (event) 
+        {
+            event.preventDefault();
+            let liElement=event.target.parentNode;
+            let ulElement=event.currentTarget;
+            ulElement.removeChild(liElement);
+
+        });
