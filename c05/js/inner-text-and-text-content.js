a1183b9c394b20e5ac6039af82980f971741191b
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-15 09:57:11 +0800

    使用.json檔指定修改name和url 增加中斷點已執行偵錯

---
+++ c05/js/inner-text-and-text-content.js
@@ -0,0 +1,7 @@
+//繼承關係
+//使用.json檔指定修改name和url 增加中斷點已執行偵錯
+
+let ElementOne = document.getElementById('one')
+let innerHTML_string =ElementOne.innerHTML
+let innerText_string=ElementOne.innerText
+let textContent_string=ElementOne.textContent
