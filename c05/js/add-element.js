663cf9f51b57174f121129a2565476bdda427365
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-15 11:03:32 +0800

    新增一個 li

---
+++ c05/js/add-element.js
@@ -0,0 +1,6 @@
+//新增一個<lI>
+let newLiElement =document.createElement('li')
+let newTextNode = document.createTextNode("新項目")
+newLiElement.appendChild(newTextNode)
+var ulElement=document.getElementsByTagName('ul')[0]
+ulElement.appendChild(newLiElement)
