2c7b090cd24c55f7b2ac056495a330a3ee099b0a
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-15 10:05:41 +0800

    透過js來新增2行<p></p> + 更改內容

--- c05/js/inner-text-and-text-content.js
+++ c05/js/inner-text-and-text-content.js
@@ -5,3 +5,12 @@ let ElementOne = document.getElementById('one')
 let innerHTML_string =ElementOne.innerHTML
 let innerText_string=ElementOne.innerText
 let textContent_string=ElementOne.textContent
+
+//透過js來新增2行<p></p>
+let msg ='<p>textContent:' + textContent_string + '</p>'
+msg += '<p>innerText:'+innerHTML_string+'</p>'
+
+//更改內容
+let ElementResults=document.getElementById('scriptResults')
+ElementResults.innerHTML=msg
+ElementOne.textContent ='被取代了'
