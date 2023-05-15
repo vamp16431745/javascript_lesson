3b194a697df59be6ad22f122d9e7d06f9c0b8e2a
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-15 14:39:52 +0800

    把事件當成參數寫進去

---
+++ c06/js/event-listener-with-parameters.js
@@ -0,0 +1,16 @@
+//event listener with parameter
+//document.getElementById('username').addEventListener('keyup',function(event){console.log(event.target.value)})
+
+
+
+document.getElementById('username').addEventListener('blur', function (event) {
+    let feedbackElement = document.getElementById('feedback')
+
+    let username = event.target.value
+    if (username.length < 5) {
+        feedbackElement.textContent = "使用者的名稱必需大於5個字元"
+    } else {
+        feedbackElement.textContent = ""
+    }
+
+}, false)
