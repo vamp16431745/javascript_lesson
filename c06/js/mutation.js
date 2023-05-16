72f34f775446c8f1541517b6b4c5827cc03fb600
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-16 14:09:18 +0800

    新創ajax資料夾

--- c06/js/mutation.js
+++ c06/js/mutation.js
@@ -1,17 +1,17 @@
 //按下Add List Item 會增加一個新的<li>
-let addElement= document.querySelector('a.add')
+let addElement = document.querySelector('a.add')
 //元素選取器(要去抓class裡的a的add)
-let ulElement=document.querySelector('#list')
+let ulElement = document.querySelector('#list')
 
 let counterElement = document.getElementById('counter')
 
-addElement.addEventListener('click',(event)=>{
+addElement.addEventListener('click', (event) => {
     console.log('click')
     event.preventDefault();  //預防點擊後連結至超連結
 
     //按下Add List Item 會增加一個新的<li>
-    let liElement=document.createElement('li')
-    let textnode=document.createTextNode("new Item")
+    let liElement = document.createElement('li')
+    let textnode = document.createTextNode("new Item")
     liElement.appendChild(textnode)  //appendChild加入某個節點  才能被看見
     ulElement.appendChild(liElement)
 
