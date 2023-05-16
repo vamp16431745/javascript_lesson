25001b0e3a3866d3e4a15ac76cc7b910295a3499
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-16 15:08:22 +0800

    把每一筆資料的站點用for each 抓出來

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -1,7 +1,13 @@
 function reqListener() {
-    let youbikedata =JSON.parse(this.responseText); 
     //把.json檔轉成javascript物件
-    console.log(youbikedata)
+    let youbikedata =JSON.parse(this.responseText); 
+    
+    //把每一筆資料的站點用for each 抓出來
+    for(const youbike of youbikedata)
+    {
+        console.log(youbike.sna)
+
+    }
 }
 
 const windowload = (event) => {
