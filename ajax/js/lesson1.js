4ddbae1d7a53ed4a98c53b91244e6d94615a5a10
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-16 15:39:34 +0800

    建立新的陣列 把重複的排除(只列出一次)

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -1,17 +1,20 @@
 function reqListener() {
     //把.json檔轉成javascript物件
-    let youbikedata =JSON.parse(this.responseText); 
-    
+    let youbikedata = JSON.parse(this.responseText);
+    let sarea_array = []
+
     //把每一筆資料的站點用for each 抓出來
-    for(const youbike of youbikedata)
-    {
-        console.log(youbike.sna)
+    for (const youbike of youbikedata) {
+        sarea_array.push(youbike.sarea)
 
     }
+    sarea_array = [...new Set(sarea_array)] //不列出重複的
+    console.log(sarea_array)
+
 }
 
 const windowload = (event) => {
-    console.log('網頁已經全部被載入');    
+    console.log('網頁已經全部被載入');
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
     req.open("GET", "	https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
