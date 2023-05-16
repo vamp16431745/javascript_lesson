7234098457ec45ff85c16902f82bbef26c2f9270
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-16 10:54:52 +0800

    建立選取到選項會出現提示警語

--- c06/js/form.js
+++ c06/js/form.js
@@ -1,6 +1,9 @@
 //建立下拉式表單
 let formElement = document.getElementById('formSignup')
 let packageElement = document.getElementById('package')
+let packageHintElement=document.getElementById('packageHint')
+let termElement=document.getElementById('terms')
+let termsHintElement=document.getElementById('termsHint')
 
 //點擊Next會在主控台顯示submitting
 formElement.addEventListener('submit', (event) => {
@@ -10,5 +13,27 @@ formElement.addEventListener('submit', (event) => {
 
 //下拉式表單選取新選項 主控台顯示changing
 packageElement.addEventListener('change', (event) => {
-    console.log('changing')    
+    console.log('changing')
+    
+    
+    
+    // for (const option of packageElement.options)
+    // {
+        // 主控台顯示表單內所有選項
+        // console.log(option.label)
+        // 顯示表單內選項的值
+        // console.log(option.value)
+    // }    
+
+    //建立選取到選項會出現提示警語
+    let selectedIndex = packageElement.selectedIndex
+    if(packageElement.options[selectedIndex].value=='monthly')
+    {
+        packageHintElement.innerText='如果選擇1年,可以省10塊美金'
+    }
+    else
+    {
+        packageHintElement.innerText='做的好'
+    }
 })
+
