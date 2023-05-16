530fa0d092e2beda6c2cbe0273e98c86058723d1
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-16 14:49:24 +0800

    modify

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -5,9 +5,8 @@ function reqListener() {
 const windowload = (event) => {
     console.log('網頁已經全部被載入');    
     const req = new XMLHttpRequest();
-    req .onreadystatechange = handleStateChange;
     req.addEventListener("load", reqListener);
-    req.open("GET", "http://www.example.org/example.txt");
+    req.open("GET", "http://www.example.org/");
     req.send();
 }
 window.addEventListener('load', windowload)
