a4fef078904f335e4ff641fac96212dc57b22741
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-16 11:53:36 +0800

    mutationObserver(追蹤 DOM 的變化，無論是子節點的變動或內容、屬性的變動，並且獲得相關的資訊，以便作出後續的行動。)

--- c06/js/mutation.js
+++ c06/js/mutation.js
@@ -3,6 +3,8 @@ let addElement= document.querySelector('a.add')
 //元素選取器(要去抓class裡的a的add)
 let ulElement=document.querySelector('#list')
 
+let counterElement = document.getElementById('counter')
+
 addElement.addEventListener('click',(event)=>{
     console.log('click')
     event.preventDefault();  //預防點擊後連結至超連結
@@ -13,5 +15,15 @@ addElement.addEventListener('click',(event)=>{
     liElement.appendChild(textnode)  //appendChild加入某個節點  才能被看見
     ulElement.appendChild(liElement)
 
-
 })
+
+
+//mutationObserver(追蹤 DOM 的變化，無論是子節點的變動或內容、屬性的變動，並且獲得相關的資訊，以便作出後續的行動。)
+const config = { attributes: true, childList: true, subtree: true };
+
+const observer = new MutationObserver((mutationsList, observer) => {
+    let listItems = ulElement.getElementsByTagName('li').length;
+    counterElement.innerText = listItems;
+});
+
+observer.observe(ulElement, config);
