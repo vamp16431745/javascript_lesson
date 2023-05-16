f48ca954f2ec92c9b0815c7874f0ed718b634da7
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-16 15:58:29 +0800

    建立下拉式選單

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -1,3 +1,7 @@
+let sareaElement = document.getElementById('sarea')
+
+
+
 function reqListener() {
     //把.json檔轉成javascript物件
     let youbikedata = JSON.parse(this.responseText);
@@ -8,16 +12,19 @@ function reqListener() {
         sarea_array.push(youbike.sarea)
 
     }
-    sarea_array = [...new Set(sarea_array)] //不列出重複的
-    console.log(sarea_array)
-
+    sarea_array = [...new Set(sarea_array)]
+    for(const area of sarea_array){
+        let optionElement = document.createElement('option')
+        optionElement.textContent = area
+        sareaElement.appendChild(optionElement)
+    }
 }
 
 const windowload = (event) => {
-    console.log('網頁已經全部被載入');
+    console.log('網頁已經全部被載入');    
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
-    req.open("GET", "	https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
+    req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
     req.send();
 }
 window.addEventListener('load', windowload)
