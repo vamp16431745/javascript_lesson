5bbb9dafab83581c66112ec1cdb36d695a1885da
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-22 11:57:05 +0800

    設定發生錯誤時的情況 類似try catch

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -43,6 +43,13 @@ sareaElement.addEventListener('change', (event) => {
 });
 
 function reqListener() {
+    console.log(this.status)
+    if(this.status != 200){
+        console.log("網頁維護中...")
+        return 
+    }
+
+
      youbikedata = JSON.parse(this.responseText)    
     for(const youbike of youbikedata){
         sarea_array.push(youbike.sarea)    
@@ -59,10 +66,21 @@ function reqListener() {
     }
 }
 
+function reqError()
+{
+    console.log("網頁維護中")
+} 
+
 const windowload = (event) => {
     console.log('網頁已經全部被載入');    
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
+    req.addEventListener("error", reqError)  //try catch  js的寫法
+     //req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
+     req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v1/youbike_immediate.json");
+     req.send();
+
+
     req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
     req.send();
 }
