103250d5838f5a0abf4f0ff4536097d6f9b71e2e
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-22 09:43:59 +0800

    modify

--- ajax/js/lesson1.js
+++ ajax/js/lesson1.js
@@ -1,45 +1,41 @@
-let sareaElement = document.getElementById('sarea')
 let sarea_array = []
+let sareaElement =  document.getElementById('sarea');
+let areaNameElement = document.getElementById('areaName')
+
 sareaElement.addEventListener('change', (event) => {
-    let selectedIndex123 = sareaElement.selectedIndex;
-    selectedValue = sareaElement.options[selectedIndex123].value
-    if(sarea_array.includes(selectedValue))
-    {
-        console.log(`行政區:${selectedValue}`)
-    } 
-
-}
-)
-
-
+    let selectedIndex = sareaElement.selectedIndex;
+    selectedValue=sareaElement.options[selectedIndex].value
+    if(sarea_array.includes(selectedValue)){
+        //console.log(`行政區:${selectedValue}`)
+        areaNameElement.innerText = selectedValue
+    }
+});
 
 function reqListener() {
-    //把.json檔轉成javascript物件
-    let youbikedata = JSON.parse(this.responseText)
-
-
-    //把每一筆資料的站點用for each 抓出來
-    for (const youbike of youbikedata) {
-        sarea_array.push(youbike.sarea)
-
+    let youbikedata = JSON.parse(this.responseText)    
+    for(const youbike of youbikedata){
+        sarea_array.push(youbike.sarea)    
     }
     sarea_array = [...new Set(sarea_array)]
     let optionElement = document.createElement('option')
     optionElement.textContent = '請選擇行政區'
-    optionElement.setAttribute('value',area)
     sareaElement.appendChild(optionElement)
-    for (const area of sarea_array) {
+    for(const area of sarea_array){
         let optionElement = document.createElement('option')
         optionElement.textContent = area
+        optionElement.setAttribute('value',area)
         sareaElement.appendChild(optionElement)
     }
 }
 
 const windowload = (event) => {
-    console.log('網頁已經全部被載入');
+    console.log('網頁已經全部被載入');    
     const req = new XMLHttpRequest();
     req.addEventListener("load", reqListener);
     req.open("GET", "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json");
     req.send();
 }
+
 window.addEventListener('load', windowload)
+
+
