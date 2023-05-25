6cfd0573cf7326697ebeb60265594cda44b6f320
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-25 16:44:49 +0800

    第2欄先確保資料為空時 會出現格式錯誤

--- project/public/js/index.js
+++ project/public/js/index.js
@@ -15,17 +15,36 @@ const validateName = ()=>{
     allOfDataArray.push({'productName':productName})
 }
 
-const clearAllAlert=()=>{
+const validateCodeFormat = () =>{
+    let inputCodeElement = document.querySelector('#inputCode')
+    let codeAlertElement = document.querySelector('#codeAlert')
+    let inputCodeValue = inputCodeElement.value
+    console.log(inputCodeValue)
+    if (inputCodeValue.length == 0){
+        //console.log("沒有填資料")
+        codeAlertElement.classList.remove("close")
+        return
+    }
+    allOfDataArray.push({'code':inputCodeValue})
+}
+
+const clearAllAlertAndData = ()=>{
     //清除產品警告
     let nameAlertElement = document.querySelector('#nameAlert')
     nameAlertElement.classList.add("close")
+
+    //清除code警告
+    let codeAlertElement = document.querySelector('#codeAlert')
+    codeAlertElement.classList.add("close")
+
     //清除收集的資料
-    allOfDataArray=[]
+    allOfDataArray = []
 }
 
 formElement.addEventListener('submit',(event)=>{
-    clearAllAlert()
+    clearAllAlertAndData()
     event.preventDefault()
     validateName()
+    validateCodeFormat()
     console.log(allOfDataArray)
 })
