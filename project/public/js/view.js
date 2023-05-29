768b5ac18cbe87df63f1af41be385f312ced91bb
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-29 16:50:45 +0800

    modify

--- project/public/js/view.js
+++ project/public/js/view.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js'
 
-import { getFirestore,collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js'
+import { getFirestore, collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js'
 
 
 const firebaseConfig = {
@@ -18,8 +18,20 @@ const db = getFirestore(app);
 
 const querySnapshot = await getDocs(collection(db, "products"));
 
+let contents = ""
 querySnapshot.forEach(doc => {
-    console.log(doc.id)
+    console.log(doc.id)    
     let documentData = doc.data()
     console.log(documentData['productName'])
+    contents += `<tr><th scope="row">${doc.id}</th><td>${documentData['productName']}</td><td>${documentData['code']}</td></tr>`
 });
+
+let tbodyElement = document.querySelector('#tbody')
+tbodyElement.innerHTML = contents
+
+
+
+
+
+
+
