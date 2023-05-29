b4e2acc4c7862e1b5f95eab43a926d12171fe4cc
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-29 16:02:22 +0800

    套用bootStrap的表格內容後 修改內容

---
+++ project/public/js/view.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js'
+
+import { getFirestore,collection, getDocs } from 'https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCGZje3uuD-fjgKXGK1gIkiAWJAU1a9x5g",
+    authDomain: "project-javascript-6b53d.firebaseapp.com",
+    projectId: "project-javascript-6b53d",
+    storageBucket: "project-javascript-6b53d.appspot.com",
+    messagingSenderId: "1042869709719",
+    appId: "1:1042869709719:web:7f407b032a60861dcfb2d2"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const querySnapshot = await getDocs(collection(db, "products"));
+
+querySnapshot.forEach(doc => {
+    console.log(doc.id)
+    let documentData = doc.data()
+    console.log(documentData['productName'])
+});
