5e31f3ae9fe86a5792842a1294898e547b3b143b
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-29 09:52:14 +0800

    定義codePatternReg 格式為xxx-xxx-xxx的 正規則表達式  來驗證產品編號

--- project/public/js/index.js
+++ project/public/js/index.js
@@ -3,32 +3,35 @@ let formElement = document.querySelector('#form')
 
 let allOfDataArray = [];
 
-const validateName = ()=>{
+const validateName = () => {
     let inputNameElement = document.querySelector('#inputName')
     let nameAlertElement = document.querySelector('#nameAlert')
     let productName = inputNameElement.value
-    if (productName.length == 0){
+    if (productName.length == 0) {
         //console.log("沒有填資料")
         nameAlertElement.classList.remove("close")
         return
     }
-    allOfDataArray.push({'productName':productName})
+    allOfDataArray.push({ 'productName': productName })
 }
 
-const validateCodeFormat = () =>{
+const validateCodeFormat = () => {
     let inputCodeElement = document.querySelector('#inputCode')
     let codeAlertElement = document.querySelector('#codeAlert')
     let inputCodeValue = inputCodeElement.value
+    const codePatternReg = /\w\w\w-\w\w\w-\w\w\w/g  
+    //正規則表達式Regular expression 能驗證xxx-xxx-xxx的語法
     console.log(inputCodeValue)
-    if (inputCodeValue.length == 0){
+    if (inputCodeValue.length == 0 || codePatternReg.test(inputCodeValue) == false
+    ) {
         //console.log("沒有填資料")
         codeAlertElement.classList.remove("close")
         return
     }
-    allOfDataArray.push({'code':inputCodeValue})
+    allOfDataArray.push({ 'code': inputCodeValue })
 }
 
-const clearAllAlertAndData = ()=>{
+const clearAllAlertAndData = () => {
     //清除產品警告
     let nameAlertElement = document.querySelector('#nameAlert')
     nameAlertElement.classList.add("close")
@@ -41,7 +44,7 @@ const clearAllAlertAndData = ()=>{
     allOfDataArray = []
 }
 
-formElement.addEventListener('submit',(event)=>{
+formElement.addEventListener('submit', (event) => {
     clearAllAlertAndData()
     event.preventDefault()
     validateName()
