bb91bdf597ef674891f115701239384a171d0a10
vamp16431745 <79435806+vamp16431745@users.noreply.github.com>
2023-05-30 15:14:27 +0800

    fetch 過去的證交所紀錄

--- project/public/js/stock.js
+++ project/public/js/stock.js
@@ -14,3 +14,31 @@ fetch(new Request('codeSearch.json'))
             selectedElement.appendChild(optionElement)
         });
     })
+
+let formElement = document.querySelector('form')
+
+formElement.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    //取得股票名稱
+    let stockId = selectedElement.value
+    if (stockId.length>4){
+        return
+    }
+
+    //取得年月份
+    let monthElement = document.querySelector('input[type="month"]')
+    let fullDateString = monthElement.value
+    fullDateString = fullDateString.replace("-","")
+    fullDateString += "01"
+    //整合網址
+    let url=`https://www.twse.com.tw/exchangeReport/STOCK_DAY?response=json&date=${fullDateString}&stockNo=${stockId}`
+
+    console.log(url)
+    fetch(new Request(url),{'mode':'cors'})
+    .then((response) => {
+        return response.json()
+    }).then((r) => {
+        console.log(r)
+    })
+
+})
